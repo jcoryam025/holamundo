7c256ae4ce15a0dd441da6f52537c52e19d58e53
johncordoba <115530666+johncordoba@users.noreply.github.com>
2022-11-03 14:05:02 +0100

    Add files via upload

---
+++ parimpar.java
@@ -0,0 +1,12 @@
+public class parimpar {
+    static int num = 13;
+    static int comp = (num % 2);
+    public static void main(String[] args) {
+        if (comp == 0) {
+            System.out.println("el número " + num + " es par");
+        } else {
+            System.out.println("el número " + num + " es impar");
+        }
+    
+    }
+}
