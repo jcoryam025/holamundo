0dbc0a5aa792a65aea74e2312dc981b512994e26
johncordoba <115530666+johncordoba@users.noreply.github.com>
2022-11-03 14:06:13 +0100

    Add files via upload

---
+++ imprimir.java
@@ -0,0 +1,11 @@
+public class imprimir {
+    static String nombre = "John";
+    static int edad = 18;
+    public static void main(String[] args) {
+        System.out.println("hola");
+        System.out.println("Hola, me llamo " + nombre + " y tengo " + edad + " años");
+        System.out.println("Nombre: " + nombre +  "  Edad: " + edad);
+        System.out.println("\"Primero/Segundo/Tercero\"");
+        
+    }
+}
